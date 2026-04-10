17c01f570f926b99522132a989541cfd5e2710d1
codephoenix86 <nareshlohar066@gmail.com>
2026-04-10 20:58:53 +0000

    feat: wire S3 client logger for request errors

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,7 +1,29 @@
 const { S3Client } = require('@aws-sdk/client-s3')
 const env = require('./env')
+const logger = require('./logger')
 
 let client = null
+
+/** AWS SDK logger plugin: log failures like Redis/Postgres; skip success info (noisy per request). */
+const sdkLogger = {
+  trace: () => {},
+  debug: () => {},
+  info: () => {},
+  warn: (content) => {
+    logger.warn('S3 client', typeof content === 'object' ? content : { message: content })
+  },
+  error: (content) => {
+    const err = content && typeof content === 'object' ? content.error : null
+    logger.error('S3 request error', {
+      clientName: content?.clientName,
+      commandName: content?.commandName,
+      metadata: content?.metadata,
+      error: err?.message,
+      stack: err?.stack,
+      name: err?.name,
+    })
+  },
+}
 
 const getClient = () => {
   if (!env.S3_ENABLED) {
@@ -14,6 +36,7 @@ const getClient = () => {
         accessKeyId: env.AWS_ACCESS_KEY_ID,
         secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
       },
+      logger: sdkLogger,
     })
   }
   return client
