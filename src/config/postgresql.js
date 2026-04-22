e0341a1293318351682c5f86bef6f90bcf7b8071
codephoenix86 <nareshlohar066@gmail.com>
2026-04-22 19:11:17 +0000

    fix: add SSL support and connection timeout to PostgreSQL pool config

--- src/config/postgresql.js
+++ src/config/postgresql.js
@@ -4,17 +4,20 @@ const env = require('./env')
 
 let pool = null
 
+const isLocalUri = (uri) => /localhost|127\.0\.0\.1/.test(uri)
+
 const getPool = () => {
   if (!pool || pool.ended) {
     pool = new Pool({
       connectionString: env.POSTGRES_URI,
       max: env.POSTGRES_POOL_MAX,
       idleTimeoutMillis: env.POSTGRES_POOL_IDLE_TIMEOUT_MS,
+      ssl: isLocalUri(env.POSTGRES_URI) ? false : { rejectUnauthorized: false },
+      connectionTimeoutMillis: 15000,
     })
     pool.on('error', (err) => logger.error('PostgreSQL pool error', { err }))
   }
   return pool
 }
 
-// Export the pool so your repositories/controllers can use it to query
 module.exports = { getPool }
