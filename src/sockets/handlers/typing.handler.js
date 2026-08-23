916574bdf310b4bdcbfd6f736dd4023767f96aa5
codephoenix86 <nareshlohar066@gmail.com>
2026-08-23 08:10:43 +0000

    fix: update socket room name format for typing events

--- src/sockets/handlers/typing.handler.js
+++ src/sockets/handlers/typing.handler.js
@@ -2,10 +2,10 @@ const { SOCKET_EVENTS } = require('@constants')
 
 module.exports = (io, socket) => {
   socket.on(SOCKET_EVENTS.TYPING_START, ({ chatId }) => {
-    socket.to(chatId).emit(SOCKET_EVENTS.TYPING_START, { userId: socket.userId, chatId })
+    socket.to(`chat:${chatId}`).emit(SOCKET_EVENTS.TYPING_START, { userId: socket.userId, chatId })
   })
 
   socket.on(SOCKET_EVENTS.TYPING_STOP, ({ chatId }) => {
-    socket.to(chatId).emit(SOCKET_EVENTS.TYPING_STOP, { userId: socket.userId, chatId })
+    socket.to(`chat:${chatId}`).emit(SOCKET_EVENTS.TYPING_STOP, { userId: socket.userId, chatId })
   })
 }
